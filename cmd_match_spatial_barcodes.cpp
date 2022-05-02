8f966c175fb630cb634bd1ee0113fa3a0b86310f
Hyun Min Kang <hmkang@umich.edu>
2022-05-02 14:20:55 -0400

    Updated match-sbcds to output unique reads

--- cmd_match_spatial_barcodes.cpp
+++ cmd_match_spatial_barcodes.cpp
@@ -7,6 +7,7 @@
 #include <set>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <algorithm>
 
 // open all tiles
 void open_tiles(dataframe_t& df, std::vector<std::string>& tiles, std::vector<tsv_reader*>& bcdfs) {
@@ -26,24 +27,10 @@ void open_tiles(dataframe_t& df, std::vector<std::string>& tiles, std::vector<ts
       error("ERROR: Observed an empty barcode file %s", df.get_str_elem(i,icol).c_str());      
     }    
   }
-
-  /*
-  tsv_reader idxf((bcddir + "manifest.tsv").c_str());
-  if ( idxf.read_line() == 0 ) // read the header
-    error("Cannot read the header of %smanifest.tsv", idxf);
-
-  while( idxf.read_line() > 0 ) { // read each line
-    std::string fn = bcddir + idxf.str_field_at(1);
-    tiles.push_back(idxf.str_field_at(0));
-    bcdfs.push_back(new tsv_reader(fn.c_str()));
-    if ( bcdfs.back()->read_line() == 0 ) {
-      error("ERROR: Observed an empty barcode file %s", fn.c_str());      
-    }
-  }
-  idxf.close();*/
 }
 
-uint64_t count_matches(std::vector<uint64_t>& bseqs, dataframe_t& df, std::vector<uint64_t>& ucounts, std::vector<uint64_t>& dcounts, int32_t match_len, htsFile* wmatch) {
+uint64_t count_matches(std::vector<uint64_t>& bseqs, dataframe_t& df, std::vector<uint64_t>& dcounts, int32_t match_len, htsFile* wmatch) {
+  //uint64_t count_matches(std::vector<uint64_t>& bseqs, dataframe_t& df, std::vector<uint64_t>& ucounts, std::vector<uint64_t>& dcounts, int32_t match_len, htsFile* wmatch) {  
 //uint64_t count_matches(std::vector<std::string>& bseqs, std::string& bcddir, std::vector<uint64_t>& counts) {
   std::vector<std::string> tiles;
   std::vector<tsv_reader*> bcdfs;
@@ -51,31 +38,17 @@ uint64_t count_matches(std::vector<uint64_t>& bseqs, dataframe_t& df, std::vecto
   open_tiles(df, tiles, bcdfs);
 
   int32_t ntiles = (int32_t)tiles.size();
-  if ( ucounts.empty() ) {
-    ucounts.resize(ntiles, 0);
+  if ( dcounts.empty() ) {
+    //ucounts.resize(ntiles, 0);
     dcounts.resize(ntiles, 0);
   }
   int32_t len = strlen(bcdfs[0]->str_field_at(0));
-  //if ( len != (int32_t) bseqs[0].size() )
-  //  error("HDMI length %d does not match to the parameters %zu", len, bseqs[0].size());  
   if ( len < match_len )
     error("HDMI length %d does not match to the parameters %d", len, match_len);
 
-  /*
-  char zzz[65535];
-  for(int32_t i=0; i < len; ++i) {
-    zzz[i] = 'Z';
-  }
-  zzz[len] = '\0';
-  */
-
   std::vector<uint64_t> tseqs(ntiles);
-  //std::vector<std::string> tseqs;
   for(int32_t i=0; i < ntiles; ++i) {
     tseqs[i] = seq2nt5(bcdfs[i]->str_field_at(0),match_len);    
-    //tseqs[i] = seq2bits(bcdfs[i]->str_field_at(0),len);
-    //tseqs.push_back(bcdfs[i]->str_field_at(0));
-    //notice("%d\t%032llu", i, tseqs[i]);
   }
 
   // sort the batch of sequences
@@ -92,25 +65,17 @@ uint64_t count_matches(std::vector<uint64_t>& bseqs, dataframe_t& df, std::vecto
   // count the sequences that matches
   for(uint64_t i=0; i < nseqs; ++i) {
     if (i % (batch_size / 20) == 0)
+      
       notice("Processing %d records, nmiss = %llu, ndups = %llu, bseqs[i]=%032llu, tseqs[0]=%032llu", i, nmiss, ndups, bseqs[i], tseqs[0]);
-    //if ( ( i > 0 ) && ( bseqs[i] == bseqs[i-1] ) ) { // avoid double-counting duplicate spatial barcodes
-    //   ++ndups;
-    //continue;
-    //}
+    
     has_match = false;
     is_dup = false;
-    //const std::string& s = bseqs[i];
     uint64_t s = bseqs[i];
     for(int32_t j=0; j < ntiles; ++j) {
       cmp = s < tseqs[j] ? -1 : ( s == tseqs[j] ? 0 : 1 );
-      //cmp = s.compare(tseqs[j]);
       while( cmp > 0 ) {
-        //if ( bcdfs[j]->read_line() == 0 ) tseqs[j].assign(zzz);
-        //else tseqs[j].assign(bcdfs[j]->str_field_at(0));
         if ( bcdfs[j]->read_line() == 0 ) tseqs[j] = UINT64_MAX;
         else tseqs[j] = seq2nt5(bcdfs[j]->str_field_at(0),match_len);
-        //else tseqs[j] = seq2bits(bcdfs[j]->str_field_at(0),len);        
-        //cmp = s.compare(tseqs[j]);
         cmp = s < tseqs[j] ? -1 : ( s == tseqs[j] ? 0 : 1 );        
       }
       if ( cmp == 0 ) {
@@ -123,9 +88,11 @@ uint64_t count_matches(std::vector<uint64_t>& bseqs, dataframe_t& df, std::vecto
         if ( ( i > 0 ) && ( bseqs[i] == bseqs[i-1] ) ) {
           is_dup = true;
         }
+        /*
         else {
           ++ucounts[j];
         }
+        */
       }
     }
     if ( is_dup ) {
@@ -184,6 +151,7 @@ int32_t cmdMatchSpatialBarcodes(int32_t argc, char** argv) {
 
   // read the manifest file
   dataframe_t df((bcddir + "manifest.tsv").c_str());
+  
   // add a column to indicate the full path
   int32_t icol = df.add_empty_column("fullpath");
   int32_t jcol = df.get_colidx("filepath");
@@ -191,20 +159,25 @@ int32_t cmdMatchSpatialBarcodes(int32_t argc, char** argv) {
     df.set_str_elem((bcddir + df.get_str_elem(i, jcol)).c_str(), i, icol);
   }
 
-  htsFile* wmatch = hts_open((outprefix + ".match.tsv.gz").c_str(), "wz");
+  // write output files as plain files (per batch), and merge them later on
+  std::vector<std::string> batch_filenames;
+  int32_t cur_batch = 0;
+
+  char buf[65536];
+  sprintf(buf, "%s.match.batch.%d.tsv", outprefix.c_str(), cur_batch);
+  htsFile* wmatch = hts_open(buf, "w");
   if ( wmatch == NULL )
-    error("Cannot open %s.matches.tsv.gz for writing", outprefix.c_str());
+    error("Cannot open %s for writing", buf);
+  batch_filenames.push_back(buf);
 
   // Read the 2nd-seq FASTQ
   htsFile* hp = hts_open(fastqf.c_str(), "r");
   notice("Reading FASTQ file %s", fastqf.c_str());  
   std::vector<uint64_t> bseqs;      // store the 2nd-sequence into an array after conveting to 64bit integer
-  //std::vector<std::string> bseqs; // an exact thing to do is to store actual strings
-  std::vector<uint64_t> ucounts, dcounts;
+  //std::vector<uint64_t> ucounts, dcounts;
+  std::vector<uint64_t> dcounts;  
   uint64_t nrecs = 0, ibatch = 0;
   uint64_t nmissdups = 0;
-  //kstring_t str; str.l = str.m = 0; str.s = NULL;  
-  //int32_t lstr, lseq, ldummy, lqual;
   int32_t lstr, lseq, ldummy, lqual;  
   kstring_t str; str.l = str.m = 0; str.s = NULL;
   lstr = hts_getline(hp, KS_SEP_LINE, &str);
@@ -218,38 +191,138 @@ int32_t cmdMatchSpatialBarcodes(int32_t argc, char** argv) {
       error("Cannot parse Readname in FASTQ file %s at record=%llu. Read length is too short (%d)", fastqf.c_str(), nrecs, lseq);
     
     bseqs.push_back(seq2nt5(str.s,match_len));
-    //bseqs.push_back(str.s);
     ldummy = hts_getline(hp, KS_SEP_LINE, &str);
     lqual  = hts_getline(hp, KS_SEP_LINE, &str);
 
     if ( ++nrecs % batch_size == 0 ) {
       notice("Processing batch %d of %d sequences", ++ibatch, batch_size);
       //nmiss += count_matches(bseqs, bcddir, counts);
-      nmissdups += count_matches(bseqs, df, ucounts, dcounts, match_len, wmatch);
+      //nmissdups += count_matches(bseqs, df, ucounts, dcounts, match_len, wmatch);
+      nmissdups += count_matches(bseqs, df, dcounts, match_len, wmatch);      
       bseqs.clear();
+
+      // open a new file
+      hts_close(wmatch);
+      cur_batch = nrecs / batch_size;
+      sprintf(buf, "%s.match.batch.%d.tsv", outprefix.c_str(), cur_batch);
+      wmatch = hts_open(buf, "w");
+      batch_filenames.push_back(buf);      
+      if ( wmatch == NULL )
+        error("Cannot open %s.matches.tsv.gz for writing", outprefix.c_str());      
     }
     lstr = hts_getline(hp, KS_SEP_LINE, &str);    
   }
   if ( bseqs.size() > 0 ) {
     notice("Processing the last batch %d containing %zu sequences", ++ibatch, bseqs.size());
-    //nmiss += count_matches(bseqs, bcddir, counts);
-    nmissdups += count_matches(bseqs, df, ucounts, dcounts, match_len, wmatch);
+    //nmissdups += count_matches(bseqs, df, ucounts, dcounts, match_len, wmatch);
+    nmissdups += count_matches(bseqs, df, dcounts, match_len, wmatch);    
     bseqs.clear();
   }
-  hts_close(wmatch);  
+  hts_close(wmatch);
+
+  // merge across batches and identify unique sequences
+  std::vector<tsv_reader*> batch_trs;
+  int32_t nbatches = (int32_t)batch_filenames.size();
+  for(int32_t i=0; i < nbatches; ++i) {
+    batch_trs.push_back(new tsv_reader(batch_filenames[i].c_str()));
+    batch_trs.back()->read_line();
+  }
+
+  htsFile* wmerged = hts_open((outprefix+".match.sorted.uniq.tsv.gz").c_str(), "wz");
+  int32_t ndups = 0, nEOFs = 0;
+  std::vector<int32_t> cmps(nbatches, 0);
+  std::vector<int32_t> vals(5);
+  std::map< int32_t, std::map<int32_t, uint64_t> > uniq_cnts;
+  int32_t i, j, k;
+  // find the new minimum
+  strcpy(buf, batch_trs[0]->str_field_at(0));
+  cmps[0] = 0;
+  for(k=0; k < 5; ++k) vals[k] = batch_trs[0]->int_field_at(k+1);
+  
+  for(i=1; i < nbatches; ++i) {
+    cmps[i] = strcmp(batch_trs[i]->str_field_at(0), buf);
+    if ( cmps[i] < 0 ) { // new minimum found
+      for(j=0; j < i; ++j)
+        cmps[j] = 1;
+      cmps[i] = 0;
+      strcpy(buf, batch_trs[i]->str_field_at(0));
+      for(k=0; k < 5; ++k) vals[k] = batch_trs[i]->int_field_at(k+1);      
+    }
+  }
+
+  while( nEOFs < nbatches ) {
+    for(i=0; i < nbatches; ++i) {
+      while( cmps[i] == 0 ) {  // keep counting duplicates
+        ++ndups;
+        if ( batch_trs[i]->read_line() > 0 ) { // next line exists
+          cmps[i] = strcmp(batch_trs[i]->str_field_at(0), buf);
+        }
+        else { // next line does not exists
+          cmps[i] = 999; // EOF reached
+          ++nEOFs;
+        }
+      }
+    }
+    // print the output
+    hprintf(wmerged, "%s\t%d\t%d\t%d\t%d\t%d\t%d\n", buf, vals[0], vals[1], vals[2], vals[3], vals[4], ndups);
+    ++uniq_cnts[vals[0]][vals[1]];
+    ndups = 0;
+
+    // determine the next minimum
+    buf[0] = '\0';
+    for(i=0; i < nbatches; ++i) {
+      if ( cmps[i] != 999 ) { // not reached EOF yet
+        cmps[i] = strcmp(batch_trs[i]->str_field_at(0), buf);
+        if ( buf[0] == '\0' || cmps[i] < 0 ) { // new minimum found
+          for(j=0; j < i; ++j) {
+            if ( cmps[j] == 0 ) {
+              cmps[j] = 1;
+            }
+          }
+          cmps[i] = 0;
+          strcpy(buf, batch_trs[i]->str_field_at(0));
+          for(k=0; k < 5; ++k) vals[k] = batch_trs[i]->int_field_at(k+1);
+        }
+      }
+    }  
+  }
+  hts_close(wmerged);  
+  // make a map between ID and lane/tile
+  std::map<std::string, std::pair<int32_t,int32_t> > id2lanetile;
+  for(std::map<int32_t, std::map<int32_t,uint64_t> >::iterator it1=uniq_cnts.begin(); it1 != uniq_cnts.end(); ++it1) {
+    for(std::map<int32_t,uint64_t>::iterator it2=it1->second.begin(); it2 != it1->second.end(); ++it2) {
+      sprintf(buf,"%d_%d", it1->first, it2->first);
+      id2lanetile[buf] = std::make_pair(it1->first, it2->first);
+    }
+  }
 
   // read manifest files
   htsFile* wf = hts_open((outprefix + ".counts.tsv").c_str(), "w");
-  hprintf(wf, "id\tfilepath\tbarcodes\tmatches\tmatches_uniq_per_batch\n");
+  //hprintf(wf, "id\tfilepath\tbarcodes\tmatches\tmatches_uniq_per_batch\n");
+  hprintf(wf, "id\tfilepath\tbarcodes\tmatches\tunique\n");
+  uint64_t nuniq = 0;
   for( int32_t i=0; i < df.nrows; ++i) {
+    std::pair<int32_t,int32_t> pair = id2lanetile[df.get_str_elem(i, "id")];
     hprintf(wf, "%s", df.get_str_elem(i, "id").c_str());
     hprintf(wf, "\t%s", df.get_str_elem(i, "filepath").c_str());
     hprintf(wf, "\t%llu", df.get_uint64_elem(i, "barcodes"));
-    hprintf(wf, "\t%llu\t%llu\n", dcounts[i], ucounts[i]);
+    hprintf(wf, "\t%llu\t%llu\n", dcounts[i], uniq_cnts[pair.first][pair.second]);
+    nuniq += uniq_cnts[pair.first][pair.second];
   }
   hts_close(wf);
   
-  notice("Total = %llu, Missed + Dups = %llu (%.5f)", nrecs, nmissdups, (double)nmissdups/(double)nrecs);
+  notice("Total = %llu, Missed + Dups (approx) = %llu (%.5f), Unique = %llu (%.5lf)", nrecs, nmissdups, (double)nmissdups/(double)nrecs, nuniq, (double)nuniq/(double)nrecs);
+
+  for(i=0; i < nbatches; ++i) {
+    batch_trs[i]->close();
+    if ( remove(batch_filenames[i].c_str()) == 0 ) {
+      notice("Successfully removed %s", batch_filenames[i].c_str());        
+    }
+    else {
+      error("ERROR in removing file %s", batch_filenames[i].c_str());              
+    }
+  }
+  notice("Finished writing the merged file");
   notice("Analysis finished");
   
   return 0;
